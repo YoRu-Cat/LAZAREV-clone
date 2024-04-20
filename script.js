1c8601906c9401f26fe3b8fa2fb845df53e71d8e
YoRu Tech <tech.niko.coding@gmail.com>
2024-04-20 13:41:17 +0500

    page3 video added (with animation)

--- script.js
+++ script.js
@@ -33,26 +33,51 @@ function navAnimation() {
     });
   });
 }
-// navAnimation();
+function page2Animation() {
+  var rightElem = document.querySelectorAll(".right-elem");
+  rightElem.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to(elem.childNodes[3], {
+        opacity: 1,
+        scale: 1,
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to(elem.childNodes[3], {
+        opacity: 0,
+        scale: 0,
+      });
+    });
+    elem.addEventListener("mousemove", function (dets) {
+      gsap.to(elem.childNodes[3], {
+        x: dets.x - elem.getBoundingClientRect().x - 60,
+        y: dets.y - elem.getBoundingClientRect().y - 170,
+      });
+    });
+  });
+}
 
-var rightElem = document.querySelectorAll(".right-elem");
-rightElem.forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
-    gsap.to(elem.childNodes[3], {
+function page3VideoAnimation() {
+  var page3Center = document.querySelector(".page3-center");
+  var video = document.querySelector("#page3 video");
+
+  page3Center.addEventListener("click", function () {
+    video.play();
+    gsap.to(video, {
+      transform: "scaleX(1) scaleY(1)",
       opacity: 1,
-      scale: 1,
+      borderRadius: 0,
     });
   });
-  elem.addEventListener("mouseleave", function () {
-    gsap.to(elem.childNodes[3], {
+  video.addEventListener("click", function () {
+    video.pause();
+    gsap.to(video, {
+      transform: "scaleX(0.7) scaleY(0)",
       opacity: 0,
-      scale: 0,
+      borderRadius: "30px",
     });
   });
-  elem.addEventListener("mousemove", function (dets) {
-    gsap.to(elem.childNodes[3], {
-      x: dets.x - elem.getBoundingClientRect().x,
-      y: dets.y - elem.getBoundingClientRect().y - 150,
-    });
-  });
-});
+}
+// navAnimation();
+// page2Animation();
+page3VideoAnimation();
